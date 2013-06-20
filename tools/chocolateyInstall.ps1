022073b0ab31db7991d22bb69cb9ec0e166e5044
Kevin Costain <kevin@cwl.cc>
2013-06-19 20:29:40 -0400

    New Package - PDFEXChangeEditor

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'PDFXchangeEditor' 
+$installerType = 'MSI'
+$url = 'http://www.tracker-software.com/downloads/PDFXVE3.x86.msi'
+$url64 = 'http://www.tracker-software.com/downloads/PDFXVE3.x64.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
