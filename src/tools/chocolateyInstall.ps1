9b2df9a0f01e79781521c71851913a649cb9d16c
David Gardiner <david@gardiner.net.au>
2016-04-17 21:06:50 +0930

    Fix installed product name

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 
   # optional, highly recommended
-  softwareName  = 'PDFXChangeEditor*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'PDF-XChange Editor' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
   checksum      = '056BB302045AA236BCBC1662FEEC1896'
   checksumType  = 'md5' #default is md5, can also be sha1
   checksum64    = '27C198C23F46B70D24ED76D41AD91B1D'
