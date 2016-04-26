c8a63beb95aaa72f48cb9b167f46418643e3ae54
David Gardiner <david@gardiner.net.au>
2016-04-26 11:42:41 +0930

    Add script to automate updating/release notes
    Parse package parameters and pass to installer

---
+++ Update-Version.ps1
@@ -0,0 +1,131 @@
+function Parse-ReleaseNotes($html)
+{
+    $ul = $html.getElementById("bh-history");
+
+    # summary
+    $ul.children[0].children[0].innerText;
+    ""
+
+    # version from <a class="active" name="6.0.317.1" href="PDFXE_history.html#6.0.317.1">
+    $version = $ul.children[0].children[0].name
+
+    # 
+    $subUl = $html.getElementById("changes-$version").children[0]
+
+    $newlyAdded = New-Object System.Collections.ArrayList
+    $bugFixed = New-Object System.Collections.ArrayList
+    $changed = New-Object System.Collections.ArrayList
+
+    $allowedTags = @('#text', 'a')
+
+    foreach ($child in $subUl.children)
+    {
+        
+        $type = $child.children[0].title
+
+        # remove unwanted tags
+        [void] ($child.childNodes | Where-Object { $allowedTags -notcontains $_.nodeName } | % { $child.removeChild($_) } )
+
+        # convert links
+
+        [void] ($child.childNodes | Where-Object { $_.nodeName -eq 'A' } | % { 
+                $textNode = $ie.Document.createTextNode("[$($_.innerText)]($($_.href))")
+                $child.replaceChild($textNode, $_)
+            } 
+            
+        )
+
+        $value = ($child.innerHTML.Trim().Replace("&lt;", "<").Replace("&gt;", ">").Replace("&amp;", "&") )
+
+        switch ($type) 
+        {
+            "Newly added feature" 
+            {
+                [void] $newlyAdded.Add($value)
+            }
+            "A reported error or bug was fixed" 
+            {
+                [void] $bugFixed.Add($value)
+            }
+            "Changed, reviewed, modified feature" 
+            {
+                [void] $changed.Add($value)
+            }
+        }
+    }
+
+    if ($newlyAdded)
+    {
+        "#### Newly added feature"
+        ""
+        $newlyAdded | % { "* " + $_ }
+        ""
+    }
+
+    if ($bugFixed)
+    {
+        "#### A reported error or bug was fixed"
+        ""
+        $bugFixed | % { "* " + $_ }
+        ""
+    }
+
+    if ($changed) 
+    {
+        "#### Changed, reviewed, modified feature"
+        ""
+
+        $changed | % { "* " + $_ }
+    }
+
+}
+
+function Update-Version
+{
+    # We use IE because the web page is using JavaScript to dynamically show version release notes
+    $ie = New-Object -ComObject InternetExplorer.Application
+
+    $ie.Silent = $true
+    $ie.Navigate("http://www.tracker-software.com/PDFXE_history.html")
+    $ie.Visible = $true
+
+    while($ie.Busy) { Start-Sleep -Milliseconds 100 }
+
+
+    Start-Sleep -Milliseconds 500
+
+    $html = $ie.Document
+
+    $ul = $html.getElementById("bh-history");
+
+    # version from <a class="active" name="6.0.317.1" href="PDFXE_history.html#6.0.317.1">
+    $version = $ul.children[0].children[0].name
+
+    # Current Version:&nbsp; 4.1.3, build 20814, released Dec. 17, 2015
+    $isMatch = $content -match "Current Version:&nbsp; (?<release>\d{1,}\.\d{1,}\.\d{1,}), build (?<build>\d{1,}), released (?<month>[A-Za-z]{3})\. (?<day>[0-9]{1,2})\, (?<year>[0-9]{4})"
+
+    if ($version)
+    {
+       $releaseNotes = (Parse-ReleaseNotes $html) -join "`n"
+
+       $nuspec = Join-Path $PSScriptRoot "src/PDFXchangeEditor.nuspec"
+       $contents = [xml] (Get-Content $nuspec -Encoding Utf8)
+
+       $contents.package.metadata.version = "$version"
+       $contents.package.metadata.releaseNotes = $releaseNotes
+
+       $contents.Save($nuspec)
+
+       Write-Host
+       Write-Host "Updated nuspec. Commit this change and open a pull request to the upstream repository on GitHub!"
+
+   }
+   else
+   {
+       Write-Host "Unable to find the release on the download page. Check the regex above"
+   }
+
+    $ie.Quit()
+}
+
+Update-Version
--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -1,8 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $packageName = 'PDFXchangeEditor' 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://34e34375d0b7c22eafcf-c0a4be9b34fe09958cbea1670de70e9b.r87.cf1.rackcdn.com/EditorV6.x86.msi' # download url
-$url64      = 'http://34e34375d0b7c22eafcf-c0a4be9b34fe09958cbea1670de70e9b.r87.cf1.rackcdn.com/EditorV6.x64.msi'
+$url        = 'http://www.tracker-software.com/downloads/EditorV6.x86.msi' # download url
+$url64      = 'http://www.tracker-software.com/downloads/EditorV6.x64.msi'
+
+# http://help.tracker-software.com/EUM/default.aspx?pageid=PDFXEdit3:switches_for_msi_installers
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,10 +17,73 @@ $packageArgs = @{
 
   # optional, highly recommended
   softwareName  = 'PDF-XChange Editor' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = '6EEFC5F1629D9CB76E3283114453F089'
+
+  # Tracker don't have unique URLs for each minor release, so we can't reliably use checksums
+  checksum      = ''
   checksumType  = 'md5' #default is md5, can also be sha1
-  checksum64    = '5D3039D213A7CE0CA9DA9260E3AC2D61'
+  checksum64    = ''
   checksumType64= 'md5' #default is checksumType
 }
+
+$arguments = @{}
+
+# Now we can use the $env:chocolateyPackageParameters inside the Chocolatey package
+$packageParameters = $env:chocolateyPackageParameters
+
+# Now parse the packageParameters using good old regular expression
+if ($packageParameters) {
+    $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z]+))"
+    $option_name = 'option'
+    $value_name = 'value'
+
+    if ($packageParameters -match $match_pattern ){
+        $results = $packageParameters | Select-String $match_pattern -AllMatches
+        $results.matches | % {
+        $arguments.Add(
+            $_.Groups[$option_name].Value.Trim(),
+            $_.Groups[$value_name].Value.Trim())
+    }
+    }
+    else
+    {
+        Throw "Package Parameters were found but were invalid (REGEX Failure)"
+    }
+
+    $customArguments = @{}
+
+    if ($arguments.ContainsKey("NoDesktopShortcuts")) {
+        Write-Host "You want NoDesktopShortcuts"
+        $customArguments.Add("DESKTOP_SHORTCUTS", "0")
+    }
+
+    if ($arguments.ContainsKey("NoUpdater")) {
+        Write-Host "You want NoUpdater"
+        $customArguments.Add("NOUPDATER", "0")
+    }
+
+    if ($arguments.ContainsKey("NoViewInBrowsers")) {
+        Write-Host "You want NoViewInBrowsers"
+        $customArguments.Add("VIEW_IN_BROWSERS", "0")
+    }
+
+    if ($arguments.ContainsKey("NoSetAsDefault")) {
+        Write-Host "You want NoSetAsDefault"
+        $customArguments.Add("SET_AS_DEFAULT", "0")
+    }
+
+    if ($arguments.ContainsKey("NoProgramsMenuShortcuts")) {
+        Write-Host "You want NoProgramsMenuShortcuts"
+        $customArguments.Add("PROGRAMSMENU_SHORTCUTS", "0")
+    }
+
+} else {
+    Write-Debug "No Package Parameters Passed in"
+}
+
+if ($customArguments.Count) { 
+    $packageArgs.silentArgs += " " + (($customArguments.GetEnumerator() | % { "$($_.Name)=$($_.Value)" } ) -join " ")
+}
+
+
 
 Install-ChocolateyPackage @packageArgs
